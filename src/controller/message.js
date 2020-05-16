f0fa1740544f6918e853e3e24151059d827eb3bb
Shama Ahlawat <shama@oriserve.com>
2020-05-16 23:47:55 +0530

    notification added

--- src/controller/message.js
+++ src/controller/message.js
@@ -1,9 +1,16 @@
-import { Chat, ChannelChat, User } from '../models';
-
-export const onMessage = async (socket, io, msg) => {
+import { Chat, ChannelChat, Workspace, User, Channel, Token } from "../models";
+import { notify } from "./notify";
+export const onMessage = async(socket, io, msg) => {
     try {
-        if (msg.room_id && msg.workspace_id && msg.chat && msg.sender_id && msg.receiver_id && !msg.isChannel) {
-            // msg.token && 
+        if (
+            msg.room_id &&
+            msg.workspace_id &&
+            msg.chat &&
+            msg.sender_id &&
+            msg.receiver_id &&
+            !msg.isChannel
+        ) {
+            // msg.token &&
             // if (msg.token != socket.id) {
             //     io.to(msg.room_id).emit('message', { status: false, message: "Invalid/Missing token" });
             //     console.log("----------------------> after emmit message", { status: false, message: "Invalid/Missing token" })
@@ -12,30 +19,34 @@ export const onMessage = async (socket, io, msg) => {
             var criteria = {
                 workspaceId: msg.workspace_id,
                 $or: [{
-                    $and: [{
-                        "senderId": msg.sender_id
-                    }, {
-                        "receiverId": msg.receiver_id
-                    }]
-                }, {
-                    $and: [{
-                        "receiverId": msg.sender_id
-                    }, {
-                        "senderId": msg.receiver_id
-                    }]
-                }]
-            }
-            let chat = await Chat.findOne(criteria)
-                .populate([
-                    {
-                        path: "receiverId",
-                        select: 'firstName lastName picture'
+                        $and: [{
+                                senderId: msg.sender_id
+                            },
+                            {
+                                receiverId: msg.receiver_id
+                            }
+                        ]
                     },
                     {
-                        path: "senderId",
-                        select: 'firstName lastName picture'
-                    },
-                ])
+                        $and: [{
+                                receiverId: msg.sender_id
+                            },
+                            {
+                                senderId: msg.receiver_id
+                            }
+                        ]
+                    }
+                ]
+            };
+            let chat = await Chat.findOne(criteria).populate([{
+                    path: "receiverId",
+                    select: "firstName lastName picture"
+                },
+                {
+                    path: "senderId",
+                    select: "firstName lastName picture"
+                }
+            ]);
             let chatData = {
                 senderId: chat && chat.senderId,
                 receiverId: chat && chat.receiverId,
@@ -44,66 +55,120 @@ export const onMessage = async (socket, io, msg) => {
                 // senderId: msg.sender_id,
                 // receiverId: msg.receiver_id,
                 message: msg.chat,
-                messageType: 'text',
-                status: 'unseen',
+                messageType: "text",
+                status: "unseen",
                 createdAt: new Date().toISOString()
-            }
+            };
             let chatData1 = {
                 roomId: msg.room_id,
                 workspaceId: msg.workspace_id,
                 senderId: msg.sender_id,
                 receiverId: msg.receiver_id,
                 message: msg.chat,
-                messageType: 'text',
-                status: 'unseen',
+                messageType: "text",
+                status: "unseen",
                 createdAt: new Date().toISOString()
-            }
-            io.in(msg.room_id).emit("message", { status: true, chatData })
-            console.log("----------------------> after emmit message", { status: true, chatData })
-            delete (chatData1.createdAt)
+            };
+            io.in(msg.room_id).emit("message", { status: true, chatData });
+            console.log("----------------------> after emmit message", {
+                status: true,
+                chatData
+            });
+            delete chatData1.createdAt;
             var myData = new Chat(chatData1);
             //   myData.save()
-            let data = await myData.save()
-            console.log("lksjdfljsdf", data)
-            return
+            let data = await myData.save();
+            console.log("lksjdfljsdf", data);
+            return;
         } else {
             if (msg.isChannel) {
                 var criteria = {
                     workspaceId: msg.workspace_id,
                     senderId: msg.sender_id
+                };
+                var channel_data = await Channel.findOne({
+                    _id: msg.room_id
+                });
+                var user = await User.findOne({
+                    _id: msg.sender_id
+                });
+                var workspace = await Workspace.findOne({
+                    _id: msg.workspace_id
+                });
+                var user_ids = JSON.parse(JSON.stringify(channel_data))
+                    .memberList;
 
+                if (user_ids.length > 0) {
+                    for (let index = 0; index < user_ids.length; index++) {
+                        const ids = user_ids[index];
+                        if (msg.sender_id != ids) {
+                            let user_token = await Token.findOne({
+                                userId: ids
+                            });
+                            if (user_token) {
+                                await notify(user_token.fcmtoken, {
+                                    title: "weaverse",
+                                    body: `${user.firstName} ${
+                                        user.lastName
+                                    } sent you a ${msg.chat} in ${
+                                        channel_data.name
+                                    } channel (${workspace.name} workspace)`,
+                                    icon: "https://res.cloudinary.com/dlwatmwd3/image/upload/v1587121530/Vyorius/weaverse-webapp/img/weaverse_design_1_1.png",
+                                    type: "groupmessage"
+                                });
+                            }
+                        }
+                    }
                 }
-                let ChannelChatData = await User.findOne({ _id: msg.sender_id })
+
+                //  console.log("sdfsdfsdf")
+
                 // .populate([
                 //     {
                 //         path: "senderId",
                 //         select: 'firstName lastName picture'
                 //     },
                 // ])
-                console.log("ChannelChatData=============", ChannelChatData)
-                const { firstName, lastName, email, phone } = ChannelChatData
-                let userdata = { ...msg, firstName, lastName, email, phone, messageType: "text", message: msg.chat }
-                io.in(msg.room_id).emit("message", { status: true, chatData: userdata })
+                // console.log("ChannelChatData=============", msg);
+                let ChannelChatData = await User.findOne({
+                    _id: msg.sender_id
+                });
+                const { firstName, lastName, email, phone } = ChannelChatData;
+                let userdata = {
+                    ...msg,
+                    firstName,
+                    lastName,
+                    email,
+                    phone,
+                    messageType: "text",
+                    message: msg.chat
+                };
+                io.in(msg.room_id).emit("message", {
+                    status: true,
+                    chatData: userdata
+                });
                 let chatData1 = {
                     roomId: msg.room_id,
                     workspaceId: msg.workspace_id,
                     senderId: msg.sender_id,
                     message: msg.chat,
-                    messageType: 'text',
-                    status: 'unseen',
+                    messageType: "text",
+                    status: "unseen",
                     createdAt: new Date().toISOString()
-                }
-                delete (chatData1.createdAt)
+                };
+                delete chatData1.createdAt;
                 var myData = new Chat(chatData1);
                 //   myData.save()
-                let data = await myData.save()
-                return
+                let data = await myData.save();
+                return;
             }
-            socket.emit('message', { status: false, message: "Invalid/Missing data" });
-            console.log("----------------------> after emmit message", { status: false, message: "Invalid/Missing data" })
+            socket.emit("message", {
+                status: false,
+                message: "Invalid/Missing data"
+            });
+            // console.log("----------------------> after emmit message", { status: false, message: "Invalid/Missing data" })
         }
-
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 import CONFIG from '../config';
 
 const {
-  MONGODB_USERNAME: username,
-  MONGODB_PASSWORD: password,
-  MONGODB_HOST: host,
-  MONGODB_PORT: port,
-  MONGODB_DATABASE_NAME: databaseName,
-  MLAB_USER: mlab_user,
-  MLAB_PASSWORD: mlab_password
+    MONGODB_USERNAME: username,
+    MONGODB_PASSWORD: password,
+    MONGODB_HOST: host,
+    MONGODB_PORT: port,
+    MONGODB_DATABASE_NAME: databaseName,
+    MLAB_USER: mlab_user,
+    MLAB_PASSWORD: mlab_password
 } = CONFIG;
 
 mongoose.Promise = global.Promise;
@@ -22,9 +22,9 @@ const MONGODB_URI = `mongodb+srv://${mlab_user}:${mlab_password}@cluster0-b9rob.
 
 console.log('MONGO CONNECTION', MONGODB_URI);
 const options = {
-  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-  reconnectInterval: 500, // Reconnect every 500ms
-  useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    useNewUrlParser: true,
 };
 
 mongoose.connect(MONGODB_URI, options);
@@ -33,18 +33,18 @@ mongoose.set('useCreateIndex', true);
 
 
 mongoose.connection.on('connected', () => {
-  // eslint-disable-next-line no-console
-  console.info(`Connected to MongoDB`);
+    // eslint-disable-next-line no-console
+    console.info(`Connected to MongoDB`);
 });
 mongoose.connection.on('error', err => {
-  // eslint-disable-next-line no-console
-  console.error(`MongoDB connection error:`, err);
-  process.exit(-1);
+    // eslint-disable-next-line no-console
+    console.error(`MongoDB connection error:`, err);
+    process.exit(-1);
 });
 
 mongoose.connection.on('disconnected', () => {
-  // eslint-disable-next-line no-console
-  console.error('MongoDB disconnected');
+    // eslint-disable-next-line no-console
+    console.error('MongoDB disconnected');
 });
 
 export default mongoose;
